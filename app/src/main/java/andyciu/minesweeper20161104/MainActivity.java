0421cee2e8c71dc3948acda83e373bca073ed75c
LiLi.Tam <mhk00123@gmail.com>
2016-11-05 02:01:10 +0800

    Initial commit

---
+++ app/src/main/java/andyciu/minesweeper20161104/MainActivity.java
@@ -0,0 +1,38 @@
+package andyciu.minesweeper20161104;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity implements ImageView.OnClickListener, ImageView.OnLongClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        for(int i=1;i<=9;++i){
+            String str1="imageView"+i;
+            int resID=getResources().getIdentifier(str1,"id",getPackageName());
+            ImageView iva= (ImageView) findViewById(resID);
+            iva.setOnClickListener(this);
+            iva.setOnLongClickListener(this);
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        ImageView ivi= (ImageView) findViewById(v.getId());
+        ivi.setImageResource(android.R.drawable.checkbox_on_background);
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        ImageView ivi= (ImageView) findViewById(v.getId());
+        ivi.setImageResource(android.R.drawable.btn_dialog);
+        return true;
+    }
+}
